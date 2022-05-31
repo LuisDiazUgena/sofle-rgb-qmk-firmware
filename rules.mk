e3071291a4d4aee9b87be179848937ff123beb8a
Luis Diaz <luisdiazugena@gmail.com>
2022-05-31 16:51:38 +0200

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,18 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+NKRO_ENABLE = yes           # Enable N-Key Rollover
