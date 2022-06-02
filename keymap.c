5554ee20cfa49d39078b9c4661d300e647e45ad6
Luis Diaz <luisdiazugena@gmail.com>
2022-06-02 12:41:57 +0200

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
   [_QWERTY] = LAYOUT(
-	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_EQL, KC_LGUI, KC_LCTL, KC_LALT,KC_LOWER, KC_ENT, KC_SPC, KC_LOWER,KC_END, KC_LBRC, KC_RBRC
+	KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,KC_BSLS , KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_EQL, KC_LCTL, KC_LGUI, KC_LALT,KC_LOWER, KC_ENT, KC_SPC, KC_LOWER,KC_END, KC_LBRC, KC_RBRC
 ),
 
 /*
@@ -178,7 +178,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(
-KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_TAB, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LGUI, KC_LCTL, KC_LALT, KC_NO, KC_SPC, KC_ENT, KC_NO, KC_HOME, KC_NO, KC_NO
+KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_TAB, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_SPC, KC_ENT, KC_NO, KC_HOME, KC_NO, KC_NO
 ),
 /* RAISe
  * ,----------------------------------------.                    ,-----------------------------------------.
@@ -300,15 +300,13 @@ char layer_state_str[70];
 // QWERTY,
 // Light on inner column and underglow
 const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	{0, 1, HSV_BLACK},
-    {35+0, 1, HSV_BLACK},
-  	{1, 34, HSV_TEAL},
-	{35+1, 34, HSV_TEAL}
+	//]{0, 1, HSV_BLACK},
+  	{0, 72, HSV_TEAL}
 );
 const rgblight_segment_t PROGMEM layer_lower_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	{0, 70, HSV_PINK},
-	{0, 1, HSV_BLACK},
-    {35+0, 1, HSV_BLACK}
+	{0, 72, HSV_PINK}
+	//{0, 1, HSV_BLACK},
+    //{35+0, 1, HSV_BLACK}
 
 );
 const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
